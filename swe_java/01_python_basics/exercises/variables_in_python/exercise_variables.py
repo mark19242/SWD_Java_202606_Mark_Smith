b95dab480164fb06418b96b17bb790c52c9e2c5f
mark19242 <mark19242@gmail.com>
2026-06-02 11:50:06 -0500

    updated exercise

---
+++ swe_java/01_python_basics/exercises/variables_in_python/exercise_variables.py
@@ -0,0 +1,64 @@
+# 1. onomatopoeia
+onomatopoeia = "pop"
+print(onomatopoeia)
+print(type(onomatopoeia))
+
+# 2. lyrics
+lyrics = """Katie Casey was baseball mad,
+Had the fever and had it bad.
+Just to root for the home town crew,
+Ev'ry sou
+Katie blew.
+On a Saturday her young beau
+Called to see if she'd like to go
+To see a show, but Miss Kate said "No,
+I'll tell you what you can do:"""
+
+print(lyrics)
+
+# 3. gpa
+gpa = 3.75
+print(gpa)
+print(type(gpa))
+
+# 4. empty_var
+empty_var = None
+print(empty_var)
+print(type(empty_var))
+
+# 5. 828^282
+power_value = 828^282
+print(power_value)
+
+# 6. Expression
+expression_result = (52 - 52) + (64 / 8) + (42 % 8)
+print(expression_result)
+
+# 7. Boolean comparison
+true_value = True
+isTrue = true_value != False
+print(isTrue)
+
+# 8. String comparison
+weird_word = "Weird"
+matches_weird = weird_word == "weird"
+print(matches_weird)
+
+# 9. Explanation of false
+false = 0
+# No error, false is bad naming but it is not a kewyword otherwise protected
+
+# 10. Mathematical operations
+var1 = 35 / 7
+var2 = 15 ** 3
+var3 = var2 % var1
+var4 = var2 / var1
+
+print("var1:", var1)
+print("var2:", var2)
+print("var3:", var3)
+print("var4:", var4)
+
+print("Comparison:")
+print("Remainder =", var3)
+print("Division =", var4)
