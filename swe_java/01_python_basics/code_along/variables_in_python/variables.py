6be23577aacdd6162ff77ca4c41cdd79f34582e4
mark19242 <mark19242@gmail.com>
2026-06-01 21:05:28 -0500

    Complete Variables in Python code along

---
+++ swe_java/01_python_basics/code_along/variables_in_python/variables.py
@@ -0,0 +1,63 @@
+# Boolean
+my_boolean = True
+print(my_boolean)
+
+# Integer
+my_int = 42
+print(my_int)
+
+# Float
+my_float = 3.14159
+print(my_float)
+
+# Strings
+my_string = 'Assess'
+my_string2 = "Educate"
+
+print(my_string)
+print(my_string2)
+
+# Multi-line string
+my_multi_line_string = """Assess
+Educate
+Advance"""
+
+print(my_multi_line_string)
+
+# Type checking
+print(type(my_boolean))
+print(type(my_int))
+print(type(my_float))
+print(type(my_string))
+
+# Reassigning variables
+my_variable = 42
+print(type(my_variable))
+
+my_variable = 'test'
+print(type(my_variable))
+
+# None
+my_variable = None
+print(type(my_variable))
+
+# Expressions
+num1 = 3.14
+num2 = 2
+num3 = 16.00
+
+add_result = num1 + num2
+subtract_check = num2 - num1
+divide_check = num3 / num2
+exponent_result = num1 ** num2
+remainder_result = num3 % num2
+greater_than_check = num1 > num2
+multiple_ops_check = round((divide_check * 5) ** num1, 2)
+
+print(add_result)
+print(subtract_check)
+print(divide_check)
+print(exponent_result)
+print(remainder_result)
+print(greater_than_check)
+print(multiple_ops_check)
