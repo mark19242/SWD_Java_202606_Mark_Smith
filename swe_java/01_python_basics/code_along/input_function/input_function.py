4b561bf9de11a8fb24f4dd6c798138bef4db72dc
mark19242 <mark19242@gmail.com>
2026-06-01 21:41:32 -0500

    Complete Input Function in Python code along

---
+++ swe_java/01_python_basics/code_along/input_function/input_function.py
@@ -0,0 +1,15 @@
+# String input
+first_name = input("Please enter your first name: ")
+print(first_name)
+
+# Numerical input (stored as a string)
+Age = input("Please enter your Age: ")
+print(Age)
+
+# Check the type
+print(type(Age))
+
+# Numerical input converted to an integer
+Age = int(input("Please enter your Age: "))
+print(Age)
+print(type(Age))
