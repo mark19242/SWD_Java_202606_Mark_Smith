ab3dac955d160e700696285bf5d6ca18b86b26fe
mark19242 <mark19242@gmail.com>
2026-06-08 10:16:55 -0500

    final prize

---
+++ swe_java/01_python_basics/exercises/SA_wheel_of_fortune/config.py
@@ -0,0 +1,26 @@
+# Dictionary file location, change if needed.
+dictionaryloc = 'data/dictionary.txt'
+
+# Turn text file location, change if needed.
+turntextloc = 'data/turntext.txt'
+
+# Wheel contents for file location, change if needed.
+wheeltextloc = 'data/wheeldata.txt'
+
+# End of Round Status status file, change if needed.
+roundstatusloc = 'data/roundstatus.txt'
+
+# number of rounds in a game
+maxrounds = 3
+
+# Vowel cost
+vowelcost = 250
+
+# final prize, you fill this in
+finalprize = 10000
+
+# final round, change if needed.
+finalRoundTextLoc = 'data/finalround.txt'
+
+# added to 
+debug = False
