06bb2a33410a97c6bbd96495651bfaa0b5f43eb3
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-06-22 13:52:00 -0500

    Arrays

--- swe_java/02_java_fundamentals/exercises/lab-text-adventure-complete/src/main/java/Main.java
+++ swe_java/02_java_fundamentals/exercises/lab-text-adventure-complete/src/main/java/Main.java
@@ -7,9 +7,8 @@ public class Main {
         Scanner console = new Scanner(System.in);
         boolean keepRunning = true;
 
-        // TODO 1: Create a boolean array.
-        boolean key1 = false;
-        boolean key2 = false;
+        // TODO 1: Create a boolean array for the keys.
+        boolean[] keys = {false, false};
 
         // TODO 2: Update the menu so it has 5 doors instead of 3.
         String mainMenu = "\nYou find yourself in a room with 3 doors. Which one do you enter?" +
@@ -18,12 +17,16 @@ public class Main {
                 "\n3. The blue painted wooden door" +
                 "\nDoor Selection: ";
 
-        // TODO 3: Put the room Strings into an array.
-        String room1 = "You've entered a room lit with candles. You see a desk with find half of a key!";
-        String room1_visited = "You've entered a room lit with candles. You've been here before. There is an empty desk.";
+        // TODO 3: Put the room Strings into arrays.
+        String[] firstVisit = {
+                "You've entered a room lit with candles. You see a desk with find half of a key!",
+                "You've entered a room cold and wet. Upon further inspection you see a hole in the far wall. Something sparkles and catches your attention. It is half of a key!"
+        };
 
-        String room2 = "You've entered a room cold and wet. Upon further inspection you see a hole in the far wall. Something sparkles and catches your attention. It is half of a key!";
-        String room2_visited = "You've entered a room cold and wet. You've been here before.";
+        String[] visitedRoom = {
+                "You've entered a room lit with candles. You've been here before. There is an empty desk.",
+                "You've entered a room cold and wet. You've been here before."
+        };
 
         String room3_locked = "You've found a room with a red door... it's locked... do you have a key?";
         String room3_unlocked = "You've found a room with a red door... you've used the key you have found and open the door!";
@@ -43,32 +46,33 @@ public class Main {
 
             // TODO 7: Update the switch to use int cases instead of String cases.
             switch (direction) {
+
                 case "1":
 
                     // TODO 8: Move the room/key logic into a method.
-                    if (key1) {
-                        System.out.println(room1_visited);
+                    if (keys[0]) {
+                        System.out.println(visitedRoom[0]);
                     } else {
-                        key1 = true;
-                        System.out.println(room1);
+                        keys[0] = true;
+                        System.out.println(firstVisit[0]);
                     }
                     break;
 
                 case "2":
 
                     // TODO 9: Use the same method for this room instead of repeating the logic.
-                    if (key2) {
-                        System.out.println(room2_visited);
+                    if (keys[1]) {
+                        System.out.println(visitedRoom[1]);
                     } else {
-                        key2 = true;
-                        System.out.println(room2);
+                        keys[1] = true;
+                        System.out.println(firstVisit[1]);
                     }
                     break;
 
                 case "3":
 
                     // TODO 10: Update this locked door logic so it checks for all 4 keys.
-                    if (key1 && key2) {
+                    if (keys[0] && keys[1]) {
                         System.out.println(room3_unlocked);
                         keepRunning = false;
                     } else {
