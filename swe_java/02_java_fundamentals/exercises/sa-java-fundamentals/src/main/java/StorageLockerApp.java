9a65b9046249782f0b8287ccf82f5a98ddf4c4ed
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-06-22 19:08:57 -0500

    Add main menu navigation

--- swe_java/02_java_fundamentals/exercises/sa-java-fundamentals/src/main/java/StorageLockerApp.java
+++ swe_java/02_java_fundamentals/exercises/sa-java-fundamentals/src/main/java/StorageLockerApp.java
@@ -13,5 +13,30 @@ public class StorageLockerApp {
 
     private static void displayMainMenu(Scanner inputScanner, String[] lockers) {
 
+        boolean keepRunning = true;
+
+        while (keepRunning) {
+            System.out.println("\nWhat would you like to do next?");
+            System.out.println("1. Rent a Locker");
+            System.out.println("2. Access a Locker");
+            System.out.println("3. Release a Locker");
+            System.out.println("---");
+            System.out.println("Any other key to exit.");
+            System.out.print("Enter choice: ");
+
+            String menuChoice = inputScanner.nextLine();
+
+            if (menuChoice.equals("1")) {
+                System.out.println("Rent a Locker selected.");
+            } else if (menuChoice.equals("2")) {
+                System.out.println("Access a Locker selected.");
+            } else if (menuChoice.equals("3")) {
+                System.out.println("Release a Locker selected.");
+            } else {
+                keepRunning = false;
+                System.out.println("Program ended.");
+            }
+        }
+
     }
 }
