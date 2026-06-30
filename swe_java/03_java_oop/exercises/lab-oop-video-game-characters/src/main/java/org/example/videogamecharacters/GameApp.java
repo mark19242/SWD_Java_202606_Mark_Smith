896392b3436954b51fc3ce162bb00fda1da1775a
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-06-30 16:34:16 -0500

    Create GameApp with character list

--- swe_java/03_java_oop/exercises/lab-oop-video-game-characters/src/main/java/org/example/videogamecharacters/GameApp.java
+++ swe_java/03_java_oop/exercises/lab-oop-video-game-characters/src/main/java/org/example/videogamecharacters/GameApp.java
@@ -1,4 +1,23 @@
 package org.example.videogamecharacters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameApp {
+
+    public static void main(String[] args) {
+
+        List<Character> characters = new ArrayList<>();
+
+        characters.add(new Warrior("Thorn", 100, 25, "battle axe"));
+        characters.add(new Mage("Elara", 80, 30, "fireball"));
+        characters.add(new Archer("Raven", 90, 20, "poison"));
+
+        System.out.println("Video Game Character Attacks");
+        System.out.println("============================");
+
+        for (Character character : characters) {
+            character.attack();
+        }
+    }
 }
