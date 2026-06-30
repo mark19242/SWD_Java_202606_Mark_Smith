d19cf8dac04997cb18a807085699686acac6948a
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-06-30 12:05:35 -0500

    Create Mage subclass

--- swe_java/03_java_oop/exercises/lab-oop-video-game-characters/src/main/java/org/example/videogamecharacters/Mage.java
+++ swe_java/03_java_oop/exercises/lab-oop-video-game-characters/src/main/java/org/example/videogamecharacters/Mage.java
@@ -1,4 +1,26 @@
 package org.example.videogamecharacters;
 
-public class Mage {
+public class Mage extends Character {
+
+    private String spell;
+
+    public Mage(String name, int health, int attackPower, String spell) {
+        super(name, health, attackPower);
+        this.spell = spell;
+    }
+
+    public String getSpell() {
+        return spell;
+    }
+
+    public void setSpell(String spell) {
+        this.spell = spell;
+    }
+
+    @Override
+    public void attack() {
+        System.out.println(getName() + " casts " + spell
+                + " and attacks for " + getAttackPower() + " magic damage!");
+    }
 }
+
