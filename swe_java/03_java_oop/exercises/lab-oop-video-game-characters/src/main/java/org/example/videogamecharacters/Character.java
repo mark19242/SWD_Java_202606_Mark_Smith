81216744d19fba6b136dd619f5198e5a630a9371
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-06-29 21:01:14 -0500

    Create abstract Character superclass

---
+++ swe_java/03_java_oop/exercises/lab-oop-video-game-characters/src/main/java/org/example/videogamecharacters/Character.java
@@ -0,0 +1,26 @@
+public abstract class Character {
+
+    private String name;
+    private int health;
+    private int attackPower;
+
+    public Character(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public abstract void attack();
+}
