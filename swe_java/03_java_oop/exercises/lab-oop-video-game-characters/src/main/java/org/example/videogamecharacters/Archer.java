af27be043d7acbb70ba0560e10332f127d0887d8
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-06-30 16:13:45 -0500

    Create Archer subclass

--- swe_java/03_java_oop/exercises/lab-oop-video-game-characters/src/main/java/org/example/videogamecharacters/Archer.java
+++ swe_java/03_java_oop/exercises/lab-oop-video-game-characters/src/main/java/org/example/videogamecharacters/Archer.java
@@ -1,4 +1,25 @@
 package org.example.videogamecharacters;
 
-public class Archer {
+public class Archer extends Character {
+
+    private String arrowType;
+
+    public Archer(String name, int health, int attackPower, String arrowType) {
+        super(name, health, attackPower);
+        this.arrowType = arrowType;
+    }
+
+    public String getArrowType() {
+        return arrowType;
+    }
+
+    public void setArrowType(String arrowType) {
+        this.arrowType = arrowType;
+    }
+
+    @Override
+    public void attack() {
+        System.out.println(getName() + " shoots a " + arrowType
+                + " arrow and attacks for " + getAttackPower() + " damage!");
+    }
 }
