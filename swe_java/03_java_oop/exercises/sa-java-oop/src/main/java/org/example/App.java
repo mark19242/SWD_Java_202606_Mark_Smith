27179a2fbf3c206e6c236949cdb6f9c87719723f
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-07-08 15:59:58 -0500

    Add starter package structure

---
+++ swe_java/03_java_oop/exercises/sa-java-oop/src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.controller.ShoppingCartController;
+
+public class App {
+
+    public static void main(String[] args) {
+        ShoppingCartController controller = new ShoppingCartController();
+        controller.run();
+    }
+}
---
+++ swe_java/03_java_oop/exercises/sa-java-oop/src/main/java/org/example/controller/ShoppingCartController.java
@@ -0,0 +1,8 @@
+package org.example.controller;
+
+public class ShoppingCartController {
+
+    public void run() {
+        System.out.println("Shopping Cart App starting...");
+    }
+}
