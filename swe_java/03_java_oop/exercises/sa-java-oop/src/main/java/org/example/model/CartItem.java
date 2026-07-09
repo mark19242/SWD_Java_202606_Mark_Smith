796e4fb0ac250819bfc4587834a32b1a8cfad9de
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-07-08 20:19:53 -0500

    Add product and cart item models

--- swe_java/03_java_oop/exercises/sa-java-oop/src/main/java/org/example/model/CartItem.java
+++ swe_java/03_java_oop/exercises/sa-java-oop/src/main/java/org/example/model/CartItem.java
@@ -1,4 +1,43 @@
 package org.example.model;
 
+// Represents a product that has been added to the cart.
 public class CartItem {
+
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Adds more of the same product to the cart.
+    public void increaseQuantity(int amount) {
+        quantity += amount;
+    }
+
+    // Removes some quantity from this cart item.
+    public void decreaseQuantity(int amount) {
+        quantity -= amount;
+    }
+
+    // Calculates price * quantity for this one cart item.
+    public double getLineTotal() {
+        return product.getPrice() * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return product.getName()
+                + " | Qty: " + quantity
+                + " | Total: $" + String.format("%.2f", getLineTotal());
+    }
 }
--- swe_java/03_java_oop/exercises/sa-java-oop/src/main/java/org/example/model/Product.java
+++ swe_java/03_java_oop/exercises/sa-java-oop/src/main/java/org/example/model/Product.java
@@ -1,4 +1,33 @@
 package org.example.model;
 
+// Represents one product that the store can sell.
 public class Product {
+
+    private String id;
+    private String name;
+    private double price;
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Makes the product display nicely when printed.
+    @Override
+    public String toString() {
+        return id + " - " + name + " - $" + String.format("%.2f", price);
+    }
 }
