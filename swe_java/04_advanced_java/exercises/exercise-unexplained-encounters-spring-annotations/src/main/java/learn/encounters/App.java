f872b969892ca672d7511d3a52502428a2bbdaa0
LEARN Academy <learnacademy@MacBook-Air.local>
2026-07-17 12:23:21 -0500

    App clean up

--- swe_java/04_advanced_java/exercises/exercise-unexplained-encounters-spring-annotations/src/main/java/learn/encounters/App.java
+++ swe_java/04_advanced_java/exercises/exercise-unexplained-encounters-spring-annotations/src/main/java/learn/encounters/App.java
@@ -4,17 +4,13 @@ import learn.encounters.ui.Controller;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import learn.encounters.ui.Controller;
-import learn.encounters.ui.View;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @ComponentScan
 public class App {
 
     public static void main(String[] args) {
-        // The app wires itself by hand. This time, let Spring do the wiring using
-        // ANNOTATIONS (no XML).
-        //
+
         // Spring scans the project and manages the application's dependencies.
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(App.class);
@@ -22,10 +18,6 @@ public class App {
         Controller controller = context.getBean(Controller.class);
         controller.run();
 
-//        EncounterRepository repository = new EncounterFileRepository("./data/encounters.csv");
-//        EncounterService service = new EncounterService(repository);
-//        View view = new View();
-//        Controller controller = new Controller(view, service);
-//        controller.run();
+
     }
 }
