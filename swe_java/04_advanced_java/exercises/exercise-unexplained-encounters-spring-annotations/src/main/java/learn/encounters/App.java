49d671bec2f915a4fdb75a410c2c515fff0f3dca
LEARN Academy <learnacademy@MacBook-Air.local>
2026-07-17 13:58:29 -0500

    load encounter file path from properties

--- swe_java/04_advanced_java/exercises/exercise-unexplained-encounters-spring-annotations/src/main/java/learn/encounters/App.java
+++ swe_java/04_advanced_java/exercises/exercise-unexplained-encounters-spring-annotations/src/main/java/learn/encounters/App.java
@@ -4,9 +4,11 @@ import learn.encounters.ui.Controller;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 
 @ComponentScan
+@PropertySource("classpath:application.properties")
 public class App {
 
     public static void main(String[] args) {
