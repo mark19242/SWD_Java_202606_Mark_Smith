5d1edf0e444bb0cc44421897dc62a4ae8bc80359
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-07-24 16:49:14 -0500

    Add Javadocs to repository interface

--- swe_java/05_summative_assessment/java-summative-assessment/src/main/java/learn/inventory/data/InventoryRepository.java
+++ swe_java/05_summative_assessment/java-summative-assessment/src/main/java/learn/inventory/data/InventoryRepository.java
@@ -5,9 +5,31 @@ import learn.inventory.model.Product;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Defines the persistence operations required by the inventory service.
+ *
+ * <p>Implementations of this interface decide how product data is stored
+ * and retrieved. For example, one implementation may use a text file,
+ * while another could use a database.</p>
+ */
+
 public interface InventoryRepository {
 
+    /**
+     * Saves the provided products to persistent storage.
+     *
+     * @param products products to save
+     * @throws IOException if the product data cannot be written
+     */
+
     void save(List<Product> products) throws IOException;
+
+    /**
+     * Loads products from persistent storage.
+     *
+     * @return the products restored from storage
+     * @throws IOException if the product data cannot be read or is invalid
+     */
 
     List<Product> load() throws IOException;
 }
