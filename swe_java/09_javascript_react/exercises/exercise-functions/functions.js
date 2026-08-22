a410e02e90e9d4e8213c9a89b6014dc6f2ae8b2b
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-08-22 17:12:09 -0500

    Complete functions exercise part 2

--- swe_java/09_javascript_react/exercises/exercise-functions/functions.js
+++ swe_java/09_javascript_react/exercises/exercise-functions/functions.js
@@ -8,3 +8,17 @@ function greetUser(name) {
 
 greetUser("Mark")
 greetUser("Rich")
+
+// ========================================
+// Part 2: Returning Values
+// ========================================
+
+function squareNumber(number) {
+  return number * number
+}
+
+const squareOfFour = squareNumber(4)
+const squareOfSeven = squareNumber(7)
+
+console.log(squareOfFour)
+console.log(squareOfSeven)
