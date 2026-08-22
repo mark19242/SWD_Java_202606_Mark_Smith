29234485a01f9c2d9c617ab55aef57611f2729ca
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-08-22 16:38:31 -0500

    Complete conditions and loops part 2

--- swe_java/09_javascript_react/exercises/exercise-conditions-loops/script.js
+++ swe_java/09_javascript_react/exercises/exercise-conditions-loops/script.js
@@ -13,3 +13,46 @@ if (randomNumber % 2 === 0) {
 } else {
   console.log(randomNumber + " is odd.")
 }
+
+// ========================================
+// Part 2: Switch Statement - Day of the Week
+// ========================================
+
+const readline = require("readline")
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+rl.question("Enter a number from 0 to 6: ", (answer) => {
+  const dayNumber = Number(answer)
+
+  switch (dayNumber) {
+    case 0:
+      console.log("Sunday")
+      break
+    case 1:
+      console.log("Monday")
+      break
+    case 2:
+      console.log("Tuesday")
+      break
+    case 3:
+      console.log("Wednesday")
+      break
+    case 4:
+      console.log("Thursday")
+      break
+    case 5:
+      console.log("Friday")
+      break
+    case 6:
+      console.log("Saturday")
+      break
+    default:
+      console.log("Error: Please enter a number from 0 to 6.")
+  }
+
+  rl.close()
+})
