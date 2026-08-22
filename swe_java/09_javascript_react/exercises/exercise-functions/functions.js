2cc37fe919b424edeeb13ca0833d7d35acee24e6
LEARN Academy <learnacademy@MacBook-Air.lan>
2026-08-22 17:08:07 -0500

    Complete functions exercise part 1

---
+++ swe_java/09_javascript_react/exercises/exercise-functions/functions.js
@@ -0,0 +1,10 @@
+// ========================================
+// Part 1: Function Basics
+// ========================================
+
+function greetUser(name) {
+  console.log("Hello, " + name + "!")
+}
+
+greetUser("Mark")
+greetUser("Rich")
